9996866c4d438d5e78455384961efa81da2bd3b6
Sam Smith <me@samsmith.id.au>
2020-10-13 18:34:31 +1030

    you can add more than 1 to your cart :)

--- addToCart.func.php
+++ addToCart.func.php
@@ -12,16 +12,37 @@
         //connect to the database
         require_once "dbconn.inc.php";
 
+        //include func
+        require_once "getNumberOfItemsInCart.dec.php";
+
         //then write a query to add it to the database
         //okay not as simple as initially thought, we should check if its already in the cart, ill fix that later
 
-        //query used to insert
-        $sqlquery = "INSERT INTO Cart (accountName, id, quantity) VALUES (?, ?, ?)";
+        //Check if the item is already in their cart
+        if($currentQty = getNumberOfItemsInCart($_GET["id"], $conn)){
+            //Update the item row to the new quantity
 
-        //voodoo shit copied from prac 3:
-        $statement = mysqli_stmt_init($conn);
-        mysqli_stmt_prepare($statement, $sqlquery);
-        mysqli_stmt_bind_param($statement, 'sii', htmlspecialchars($_SESSION["loggedInUser"]), htmlspecialchars($_GET["id"]), $_GET["quantity"]);
+            $sqlquery = "UPDATE Cart SET quantity = quantity + ? WHERE accountName = ? AND id = ?";
+
+            //voodoo shit copied from prac 3:
+            $statement = mysqli_stmt_init($conn);
+            mysqli_stmt_prepare($statement, $sqlquery);
+            mysqli_stmt_bind_param($statement, 'isi', $_GET["quantity"], htmlspecialchars($_SESSION["loggedInUser"]), htmlspecialchars($_GET["id"]));
+        
+
+        } else {
+            //Otherwise, create a new item in the cart
+
+            //query used to insert
+            $sqlquery = "INSERT INTO Cart (accountName, id, quantity) VALUES (?, ?, ?)";
+
+            //voodoo shit copied from prac 3:
+            $statement = mysqli_stmt_init($conn);
+            mysqli_stmt_prepare($statement, $sqlquery);
+            mysqli_stmt_bind_param($statement, 'sii', htmlspecialchars($_SESSION["loggedInUser"]), htmlspecialchars($_GET["id"]), $_GET["quantity"]);
+        
+        }
+
         
         mysqli_stmt_execute($statement);
 
