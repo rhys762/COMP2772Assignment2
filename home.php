6e3f52ef622f2c6e652882327263b9792fdec0a2
mhamdorf <hamdorf.07@gmail.com>
2020-10-17 17:11:01 +1030

    Added grid layout for product images

--- home.php
+++ home.php
@@ -17,42 +17,43 @@
     <?php require_once "displayHeader.func.php"; ?>
     <!-- display hot items or something? -->
 
-    <div class=”gallery”>
-        <figure class="gallery__item gallery__item--1">
-            <img src="images/products/Vampire.png" class="gallery__img" alt="Image 1">
-        </figure>
-        <figure class="gallery__item gallery__item--2">
-            <img src="images/products/Nurse.png" class="gallery__img" alt="Image 2">
-        </figure>
-        <figure class="gallery__item gallery__item--3">
-            <img src="img/image-3.jpg" class="gallery__img" alt="Image 3">
-        </figure>
-        <figure class="gallery__item gallery__item--4">
-            <img src="img/image-4.jpg" class="gallery__img" alt="Image 4">
-        </figure>
-        <figure class="gallery__item gallery__item--5">
-            <img src="img/image-5.jpg" class="gallery__img" alt="Image 5">
-        </figure>
-        <figure class="gallery__item gallery__item--6">
-            <img src="img/image-6.jpg" class="gallery__img" alt="Image 6">
-        </figure>
-        <figure class="gallery__item gallery__item--7">
-            <img src="img/image-1.jpg" class="gallery__img" alt="Image 7">
-        </figure>
-        <figure class="gallery__item gallery__item--8">
-            <img src="img/image-2.jpg" class="gallery__img" alt="Image 8">
-        </figure>
-        <figure class="gallery__item gallery__item--9">
-            <img src="img/image-3.jpg" class="gallery__img" alt="Image 9">
-        </figure>
-        <figure class="gallery__item gallery__item--10">
-            <img src="img/image-4.jpg" class="gallery__img" alt="Image 10">
-        </figure>
-        <figure class="gallery__item gallery__item--11">
-            <img src="img/image-5.jpg" class="gallery__img" alt="Image 11">
-        </figure>
-        <figure class="gallery__item gallery__item--12">
-            <img src="img/image-6.jpg" class="gallery__img" alt="Image 12">
-        </figure>
-    </div>
+
+<div class="gallery">
+  <div class="one">
+  <img id="vampire" src="images/products/Vampire.png" alt="Vampire">
+  </div>
+  <div class="two">
+  <img id="mummy" src="images/products/Mummy.png" alt="Mummy">
+  </div>
+  <div class="three">
+  <img id="nurse" src="images/products/Nurse.png" alt="Nurse">
+  </div>
+  <div class="four">
+  <img id="frankenstein" src="images/products/Frankenstein.png" alt="Frankenstein">
+  </div>
+  <div class="five">
+  <img id="superHero" src="images/products/SuperHero.png" alt="Super Hero">
+  </div>
+  <div class="six">
+  <img id="angel" src="images/products/Angel.png" alt="Angel">
+  </div>
+  <div class="seven">
+  <img id="devil" src="images/products/Devil.png" alt="Devil">
+  </div>
+  <div class="eight">
+  <img id="evilPumpkin" src="images/products/EvilPumpkin.png" alt="Evil Pumpkin">
+  </div>
+  <div class="nine">
+  <img id="ghost" src="images/products/Ghost.png" alt="Ghost">
+  </div>
+  <div class="ten">
+  <img id="zombie" src="images/products/Zombie.png" alt="Zombie">
+  </div>
+  <div class="eleven">
+  <img id="witch" src="images/products/Witch.png" alt="Witch">
+  </div>
+  <div class="twelve">
+  <img id="headlessGhost" src="images/products/HeadlessGhost.png" alt="Headless Ghost">
+  </div>
+</div>
 </body>
