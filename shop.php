83ea833069da3dfe501a908f853473b9f0814cd1
rhys762 <rhystrueman0@gmail.com>
2020-10-18 23:25:19 +1100

    fixed disregard previous

--- shop.php
+++ shop.php
@@ -13,48 +13,7 @@
 </head>
 
 <body>
-    <!-- pull the header -->
-    <?php require_once "displayHeader.func.php"; ?>
-    <!-- display hot items or something? -->
 
-    <div class="gallery">
-        <div class="one">
-            <p><img id="vampire" src="images/products/Vampire.png" alt="Vampire"></p>
-        </div>
-        <div class="two">
-            <p><img id="mummy" src="images/products/Mummy.png" alt="Mummy"></p>
-        </div>
-        <div class="three">
-            <p><img id="nurse" src="images/products/Nurse.png" alt="Nurse"></p>
-        </div>
-        <div class="four">
-            <p><img id="frankenstein" src="images/products/Frankenstein.png" alt="Frankenstein"></p>
-        </div>
-        <div class="five">
-            <p><img id="superHero" src="images/products/SuperHero.png" alt="Super Hero"></p>
-        </div>
-        <div class="six">
-            <p><img id="angel" src="images/products/Angel.png" alt="Angel"></p>
-        </div>
-        <div class="seven">
-            <p><img id="devil" src="images/products/Devil.png" alt="Devil"></p>
-        </div>
-        <div class="eight">
-            <p><img id="evilPumpkin" src="images/products/EvilPumpkin.png" alt="Evil Pumpkin"></p>
-        </div>
-        <div class="nine">
-            <p><img id="ghost" src="images/products/Ghost.png" alt="Ghost"></p>
-        </div>
-        <div class="ten">
-            <p><img id="zombie" src="images/products/Zombie.png" alt="Zombie"></p>
-        </div>
-        <div class="eleven">
-            <p><img id="witch" src="images/products/Witch.png" alt="Witch"></p>
-        </div>
-        <div class="twelve">
-            <p><img id="headlessGhost" src="images/products/HeadlessGhost.png" alt="Headless Ghost"></p>
-        </div>
-    </div>
     <?php
         //pull the header
         require_once "displayHeader.func.php"; 
