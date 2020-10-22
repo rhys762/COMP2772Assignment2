ff780fcd365f247f61e3c9e19a7d3a4cd6470e62
mhamdorf <hamdorf.07@gmail.com>
2020-10-22 22:41:22 +1030

    abc

--- checkout3.php
+++ checkout3.php
@@ -46,7 +46,6 @@
     echo $rows_prev["suburb"] . ", " . $rows_prev["stat"] . ", " . $rows_prev["postcode"];
     echo "</h3>";
 
-<<<<<<< HEAD
     echo "</div>";
 
 ?>
@@ -54,34 +53,6 @@
 <div class="paymentForm">
     <div class="paymentHeading"><h3>Payment Details:</h3></div><br><br>
     <form action="saveCard.func.php" method="POST">
-=======
-    //Display a card validation error
-    if(isset($_SESSION['cardWrong']))
-    {
-        if($_SESSION['cardWrong'] == 1)
-        {
-            echo "<div class=errorMsg>" . "<p class =\"errorText\">Invalid card number. Please try again</p>" . "</div>";
-            //unset it so if the user navigate away and then comes back its not still there
-            unset($_SESSION['cardWrong']);
-        }
-    }
-
-    //Display a card processing error
-    if(isset($_SESSION['badPay']))
-    {
-        if($_SESSION['badPay'] == 1)
-        {
-            echo "<div class=errorMsg>" . "<p class =\"errorText\">Payment declined. Please try again, or contact your bank</p>" . "</div>";
-            //unset it so if the user navigate away and then comes back its not still there
-            unset($_SESSION['badPay']);
-        }
-    }
-
-
-?>
-    <p>Enter payment info:</p>
-    <form action="paymentGo.func.php" method="POST">
->>>>>>> eeee62122bb270a8127514ef9bb570c279c62733
         <label for="cardNo">Card Number:</label><br>
         <input type="text" name="cardNo" pattern="[0-9]*" maxlength="16" required><br>
         <label for="cardName">Name on Card:</label><br>
